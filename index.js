eea61db403abb79235df3e7fbecc5dfaa7914061
nathanmailhiot <nathanjm12@outlook.com>
2020-02-26 11:11:09 -0500

    DB connection

--- index.js
+++ index.js
@@ -7,37 +7,41 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const path = require("path");
+const cookieParser = require("cookie-parser");
 const expressValidator = require("express-validator");
+const cors = require("cors");
+const authRoutes = require("./routes/auth");
 
 /**
  * App Variables
  */
 const app = express();
-const port = process.env.PORT || "8000";
-const authRoutes = require("./routes/auth");
 
 /**
  * DB
  */
 
-// mongoose
-//   .connect(process.env.MONGO_URI, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-//   })
-//   .then(console.log("DB connected"));
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(console.log("DB connected"));
 
-// mongoose.connection.on("error", error => {
-//   console.log(`DB connection error: ${error.message}`);
-// });
+mongoose.connection.on("error", error => {
+  console.log(`DB connection error: ${error.message}`);
+});
 
 /**
  *  App Middleware
  */
 app.use(morgan("dev"));
 app.use(bodyParser.json());
+app.use(cookieParser());
 app.use(expressValidator());
+app.use(cors());
 app.use("/", authRoutes);
+
 /**
  * Routes Definitions
  */
@@ -48,6 +52,7 @@ app.get("/", (req, res) => {
 /**
  * Server Activation
  */
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`Listening to requests on http://localhost:${port}`);
 });
