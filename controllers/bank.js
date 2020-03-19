d7494b3727160d8a991132bd92b21bcd464b2c3a
nathanmailhiot <nathanjm12@outlook.com>
2020-03-19 13:35:01 -0400

    Banking Create, Update, Show

--- controllers/bank.js
+++ controllers/bank.js
@@ -25,3 +25,78 @@ exports.createBank = (req, res, next) => {
     });
   });
 };
+
+exports.bankById = (req, res, next, id) => {
+  Bank.findById(id).exec((error, bank) => {
+    if (error || !bank) {
+      return res.status(400).json({
+        error: error
+      });
+    }
+    req.bank = bank;
+    next();
+  });
+};
+
+exports.isOwner = (req, res, next) => {
+  console.log(req.profile);
+  console.log(req.auth);
+  let isOwner = req.profile && req.auth && req.profile._id == req.auth.id;
+  if (!isOwner) {
+    return res.status(403).json({
+      error: "User is not authorized"
+    });
+  }
+  next();
+};
+
+exports.bankByUser = (req, res) => {
+  Bank.find({ createdBy: req.profile._id })
+    .select("checking saving createdBy ")
+    .sort("_created")
+    .exec((error, bank) => {
+      if (error) {
+        return res.status(400).json({
+          error: error
+        });
+      }
+      res.json({ bank });
+    });
+};
+
+exports.updateBank = (req, res, next) => {
+  let form = new formidable.IncomingForm();
+  form.keepExtensions = true;
+  form.parse(req, (error, fields) => {
+    if (error) {
+      return res.status(400).json({
+        error: "bank couldn't be updated"
+      });
+    }
+    let bank = req.bank;
+    bank = _.extend(bank, fields);
+    bank.save((error, result) => {
+      if (error) {
+        return res.status(400).json({
+          error: error
+        });
+      }
+      res.json(bank);
+    });
+  });
+};
+
+exports.isCreator = (req, res, next) => {
+  let isCreator = req.bank && req.auth && req.bank.createdBy == req.auth.id;
+
+  if (!isCreator) {
+    return res.status(403).json({
+      error: "User is not authorized"
+    });
+  }
+  next();
+};
+
+exports.singleBank = (req, res) => {
+  return res.json(req.bank);
+};
--- routes/bank.js
+++ routes/bank.js
@@ -1,6 +1,14 @@
 const express = require("express");
 
-const { createBank } = require("../controllers/bank");
+const {
+  createBank,
+  bankById,
+  bankByUser,
+  updateBank,
+  singleBank,
+  isCreator,
+  isOwner
+} = require("../controllers/bank");
 
 const { requireSignin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
@@ -10,6 +18,16 @@ const router = express.Router();
 // Create bank info
 router.post("/bank/new/:userId", requireSignin, createBank);
 
+// Get Bank
+router.get("/bank/by/:userId", requireSignin, isOwner, bankByUser);
+
+// Get single Bank
+router.get("/bank/:bankId", requireSignin, isCreator, singleBank);
+
+// Update bank
+router.put("/edit/bank/:bankId", requireSignin, isCreator, updateBank);
+
 router.param("userId", userById);
+router.param("bankId", bankById);
 
 module.exports = router;
