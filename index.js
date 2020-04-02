85f5871be4af8a0aaa990b761819c7856e9a7583
nathanmailhiot <nathanjm12@outlook.com>
2020-04-02 13:52:14 -0400

    Notes updated and back on dev DB

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const app = express();
  * DB
  */
 mongoose
-  .connect(process.env.MONGO_URI_PROD, {
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
